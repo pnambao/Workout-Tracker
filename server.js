2ee6c3ce13e38318158ad94cf9b4a95c596e23cd
RoachCoach <panjenambao@gmail.com>
2021-11-01 21:48:35 -0500

    added server

---
+++ server.js
@@ -0,0 +1,34 @@
+// require express, mongoose, morgan; create mongoose database connection; create routes and listen request for port
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3004;
+
+// Create the express app
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// Mongoose database connection, OR localhost connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+
+});
+
+// the routes for api-routes.js and html-routes.js
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
+
+// Listen to the request on a port
+app.listen(PORT, () => {
+    console.log(`Hi there, Buddy! The app is listening on port ${PORT}!`);
+  });
